ae4dd8544e7fa258f251b77f7b32b6906f5fca2f
Limitless <kenon949@gmail.com>
2025-06-25 20:19:54 +0200

    scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
-    <div style={{position: 'relative', color: '#111', background: '#fff'}}>
-      <div style={{padding: 40, fontSize: 24, textAlign: 'center'}}>
-        Test scrollowania. Przewiń stronę w dół i do góry.<br />
-        Jeśli widzisz tylko jeden scroll, to znaczy, że problem leżał w kodzie.<br />
-        Jeśli nadal są dwa scrolle, to problem leży w środowisku, Next.js lub przeglądarce.<br /><br />
-        (Ten widok nie ma żadnych styli, animacji, czcionek, wrapperów, flexów, min-h-screen, ani efektów!)
+    <div className="min-h-screen flex flex-col">
+      <div style={{position: 'relative', color: '#111', background: '#fff'}}>
+        <div style={{padding: 40, fontSize: 24, textAlign: 'center'}}>
+          Test scrollowania. Przewiń stronę w dół i do góry.<br />
+          Jeśli widzisz tylko jeden scroll, to znaczy, że problem leżał w kodzie.<br />
+          Jeśli nadal są dwa scrolle, to problem leży w środowisku, Next.js lub przeglądarce.<br /><br />
+          (Ten widok ma już wrapper: min-h-screen flex flex-col)
+        </div>
+        <div style={{height: '200vh'}}></div>
       </div>
-      <div style={{height: '200vh'}}></div>
     </div>
   );
 }
